6053d397b7bc301971911d2948db86524b739244
Bineta Thiam <netapha04@gmail.com>
2018-03-14 20:21:18 +0000

    Création des Getters et Setters de la classe Employe

--- Employe.java
+++ Employe.java
@@ -4,6 +4,14 @@ private String nomEmploye,
 public Employe(){
 }
 
-public Employe(String nom){
-this.nom = nom}
+public Employe(String nomEmploye){
+this.nomEmploye = nomEmploye}
+// Getters et Setters
+public String getNomEmploye(){
+	return this.nomEmploye
+}
+
+public void setNomEmploye(String nomEmploye){
+	this.nomEmploye = nomEmploye;
+}
 }
