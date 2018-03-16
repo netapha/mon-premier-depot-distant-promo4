13536bd6ca865e1327ad7905947b9627b1e20cca
Bineta Thiam <netapha04@gmail.com>
2018-03-16 20:06:08 +0000

    Revert "Ajout des méthodes métiers dans les classe Employe et Etudiant"
    This reverts commit 3f41a00156cd45cacbb3e0b6f6a9e7e8da43739b.

--- Etudiant.java
+++ Etudiant.java
@@ -1,24 +1,15 @@
 public Etudiant{
 
-private String nomEtudiant;
-private String prenomEtudiant;
+private String nomEtudiant,
 public Etudiant(){
 }
 
 public Etudiant(String nomEtudiant){
 this.nomEtudiant = nomEtudiant}
-public Etudiant(String nomEtudiant, String prenomEtudiant){
-this.nomEtudiant = nomEtudiant
-this.prenomEtudiant = prenomEtudiant
-}
 // Getters et Setters
 public String getNomEtudiant(){
 	return this.nomEtudiant
 }
 public void setNomEtudiant(String nomEtudiant){
 	this.nomEtudiant = nomEtudiant;
-}
-public void affiche(){
-	system.out.println(this.nomEtudiant+ " "+ this.prenomEtudiant)
-}
 }
