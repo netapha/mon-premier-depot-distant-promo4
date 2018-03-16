840fcbf7bc443962fb33b7c3aac6d860895ac503
Bineta Thiam <netapha04@gmail.com>
2018-03-16 18:55:34 +0000

    Ajout d'un constructeur avec deux parametres

--- Employe.java
+++ Employe.java
@@ -1,11 +1,17 @@
 public Employe{
 
-private String nomEmploye,
+private String nomEmploye;
+private String prenomEmploye;
 public Employe(){
 }
+// Constructeur avec un parametre
 
 public Employe(String nomEmploye){
 this.nomEmploye = nomEmploye}
+
+public Employe(String nomEmploye,String prenomEmploye){
+this.nomEmploye = nomEmploye
+this.prenomEmploye = prenomEmploye}
 // Getters et Setters
 public String getNomEmploye(){
 	return this.nomEmploye
