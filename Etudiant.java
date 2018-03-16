3f41a00156cd45cacbb3e0b6f6a9e7e8da43739b
Bineta Thiam <netapha04@gmail.com>
2018-03-16 19:01:58 +0000

    Ajout des méthodes métiers dans les classe Employe et Etudiant

--- Etudiant.java
+++ Etudiant.java
@@ -1,15 +1,24 @@
 public Etudiant{
 
-private String nomEtudiant,
+private String nomEtudiant;
+private String prenomEtudiant;
 public Etudiant(){
 }
 
 public Etudiant(String nomEtudiant){
 this.nomEtudiant = nomEtudiant}
+public Etudiant(String nomEtudiant, String prenomEtudiant){
+this.nomEtudiant = nomEtudiant
+this.prenomEtudiant = prenomEtudiant
+}
 // Getters et Setters
 public String getNomEtudiant(){
 	return this.nomEtudiant
 }
 public void setNomEtudiant(String nomEtudiant){
 	this.nomEtudiant = nomEtudiant;
+}
+public void affiche(){
+	system.out.println(this.nomEtudiant+ " "+ this.prenomEtudiant)
+}
 }
